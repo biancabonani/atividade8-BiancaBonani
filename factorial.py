5358e6b071fb30feb43e5e65e7bbe241b76c1ee2
Your Name <biancabonani@gmail.com>
2025-04-04 18:21:05 +0200

    Adicionando código de cálculo de fatorial

--- factorial.py
+++ factorial.py
@@ -1,1 +1,10 @@
-print("Olá, mundo!")
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+
+# Testando o código
+num = int(input("Digite um número: "))
+print(f"O fatorial de {num} é {fatorial(num)}")
+
